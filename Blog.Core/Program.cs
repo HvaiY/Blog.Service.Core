30f04d010b4f79615b7c158517300fe0321737fb
袁龙海 <337646685@qq.com>
2019-06-09 20:20:50 +0800

    新增：WebApi Init

---
+++ Blog.Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Blog.Service.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                // .UseKestrel() //web寄宿服务  都内置了可以查看源码->CreateDefaultBuilder
+                // .UseContentRoot(Directory.GetCurrentDirectory())
+                // .UseIISIntegration() //调试用服务器
+                .UseStartup<Startup>();
+    }
+}
